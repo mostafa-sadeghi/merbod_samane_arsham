2b76881ea424c7946ed521b6d167f0594712031d
msa <msai.sadeghi@gmail.com>
2023-09-20 17:56:27 +0330

    s

---
+++ session3.py
@@ -0,0 +1,32 @@
+# x = 2
+# y = 3
+
+# print(f"x + y = {x + y}")
+# print(f"{x} + {y} = {x + y}")
+
+# name = input("enter your name: ")
+# course = input("enter your course name: ")
+
+# message = f"{name} is learning {course}"
+# print(message)
+
+
+a = int(input("enter a number: "))
+b = int(input("enter a number: "))
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+
+# TODO
+"""
+برنامه ای بنویسید که نام نام خانوادگی و شهر محل تولد شما را از ورودی
+دریافت نماید 
+و پیغامی مشابه زیر نمایش دهد
+
+artin rezaei is from tehran.
+
+"""
