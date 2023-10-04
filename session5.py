bec6873b8b6455932976e99f649ee5d59f222e75
msa <msai.sadeghi@gmail.com>
2023-10-04 18:00:39 +0330

    s

---
+++ session5.py
@@ -0,0 +1,16 @@
+number_1 = int(input("enter a number: "))
+number_2 = int(input("enter another number: "))
+
+if number_1 == number_2:
+    print(f'{number_1} = {number_2}')
+else:
+    print(f'{number_1} is not equal to {number_2}')
+
+
+# >      >=      <       <=      !=
+
+# TODO "برنامه ای بنویسید که دو نام را از ورودی دریافت نمایدو در صورت مساوی بودن
+# پیغام زیر را نمایش دهد"
+"both names are the same"
+"در غیر اینصورت پیغام زیر را نمایش دهد"
+"names are not equal"
