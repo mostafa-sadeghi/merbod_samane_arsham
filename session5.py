d581dafbc2904ad83382224708e815b4ba475720
msa <msai.sadeghi@gmail.com>
2023-10-11 18:00:36 +0330

    S

--- session5.py
+++ session5.py
@@ -1,10 +1,10 @@
-number_1 = int(input("enter a number: "))
-number_2 = int(input("enter another number: "))
+# number_1 = int(input("enter a number: "))
+# number_2 = int(input("enter another number: "))
 
-if number_1 == number_2:
-    print(f'{number_1} = {number_2}')
-else:
-    print(f'{number_1} is not equal to {number_2}')
+# if number_1 == number_2:
+#     print(f'{number_1} = {number_2}')
+# else:
+#     print(f'{number_1} is not equal to {number_2}')
 
 
 # >      >=      <       <=      !=
@@ -14,3 +14,12 @@ else:
 "both names are the same"
 "در غیر اینصورت پیغام زیر را نمایش دهد"
 "names are not equal"
+
+# name1 = input("enter your name: ")
+# name2 = input("enter another name: ")
+# if name1 == name2:
+#     print("both names are the same")
+# else:
+#     print("names are not equal")
+
+
