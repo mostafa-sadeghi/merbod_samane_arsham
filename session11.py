d764a652ad96cea8711c47041bf631fe17419439
msa <msai.sadeghi@gmail.com>
2023-11-29 17:19:57 +0330

    S

--- session11.py
+++ session11.py
@@ -13,17 +13,19 @@
 
 # تاپل قابل تغییر نیست
 
-favorite_sports = {
-    "mehrbod":"football",
-    "arsham":"tennis",
-    "samane":"basketball",
-    "sorena":"football"
-}
-print(f'sorena likes {favorite_sports["sorena"]}')
-print(f'arsham likes {favorite_sports["arsham"]}')
-print(f'mehrbod likes {favorite_sports["mehrbod"]}')
-print(f'samane likes {favorite_sports["samane"]}')
+# favorite_sports = {
+#     "mehrbod":"football",
+#     "arsham":"tennis",
+#     "samane":"basketball",
+#     "sorena":"football"
+# }
+# print(f'sorena likes {favorite_sports["sorena"]}')
+# print(f'arsham likes {favorite_sports["arsham"]}')
+# print(f'mehrbod likes {favorite_sports["mehrbod"]}')
+# print(f'samane likes {favorite_sports["samane"]}')
 
 # TODO  یک دیکشنری بسازید که غذاهای مورد علاقه چهار نفر را ذخیره کند
 # و سپس غذای مورد علاقه هریک نمایش دهد
 # اگر توانستید با اینپوت انجامش دهید
+
+
---
+++ session12.py
@@ -0,0 +1,15 @@
+favorite_foods = {}
+
+for i in range(2):
+    name = input('enter a name: ')
+    food = input('enter a food: ')
+    favorite_foods[name] = food
+
+
+name = input('enter a name: ')
+print(favorite_foods[name])
+
+
+
+# print(favorite_foods)
+
